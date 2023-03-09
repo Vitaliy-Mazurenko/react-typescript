0e21bc2efc9da0055c645e0fbaae3e3571896301
Vitalii <vit007m@gmail.com>
2023-03-09 12:39:33 +0200

    test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            <code>src/App.tsx</code> TypeScript.
+            <code>src/App.tsx</code> React+ TypeScript.
         </p>
         <a
           className="App-link"
