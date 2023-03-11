7757ad9ce84125e03077a4c7bf84097c738bb58a
Vitalii <vit007m@gmail.com>
2023-03-11 21:23:56 +0200

    test: change src/index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Table from './components/Table/Table';
-import Home from './components/Home/Home';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/table' element={ <Table/> } />
-          <Route path='/' element={ <Home /> } />
-        </Routes>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
