4cb796ab26dcf221c3be1811276484d3ac3be91a
Vitalii <vit007m@gmail.com>
2023-03-11 22:26:47 +0200

    fix: package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Table from './components/Table/Table';
+import Home from './components/Home/Home';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/table' element={ <Table/> } />
+          <Route path='/' element={ <Home /> } />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
